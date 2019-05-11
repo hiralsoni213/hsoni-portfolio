1bcde4afc08130d68e431d78245b881ebff0ecbd
Hiral Soni <hiralsoni213@gmail.com>
2019-05-11 11:54:38 -0400

    Added angular component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { HeaderComponent } from './header/header.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { InterestsComponent } from './interests/interests.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ExperienceComponent,
+    HeaderComponent,
+    EducationComponent,
+    SkillsComponent,
+    InterestsComponent
   ],
   imports: [
     BrowserModule
